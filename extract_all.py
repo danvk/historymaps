dd6af3352747258f3f7d22ce61dfeb092e83b4a8
Dan Vanderkam <dan@dygraphs.com>
2011-12-26 17:39:23 -0500

    Able to parse all 2510 SVG files (not sure how correctly yet)

--- extract_all.py
+++ extract_all.py
@@ -5,9 +5,12 @@ import re
 import whm
 
 
-ordered_files = whm.ordered_map_files()
+#ordered_files = whm.ordered_map_files()
+fs = whm.all_ordered_map_files()
 
-for f in ordered_files:
+#fs = [f for f in fs if whm.filename_to_year(f) >= 1763]
+
+for f in fs:
   svg = whm.SvgFile(f)
   print '%+4d: %d' % (svg.year(), len(svg.countries()))
 
--- whm.py
+++ whm.py
@@ -21,12 +21,19 @@ def ordered_map_files(html_dir="worldhistorymaps/html"):
   return sorted(glob.glob(html_dir + '/W?????.svg'), key=filename_to_year)
 
 
+def all_ordered_map_files():
+  return sorted(ordered_map_files() +
+                ordered_map_files('worldhistorymaps/html/off-years'),
+                key=filename_to_year)
+
+
 def parse_year_range(year_range):
   """Takes "232 - 25BCE" and returns [-232, -25]."""
   # note: this eliminates any uncertainty, like 'c500BCE - c200'.
   year_range = year_range.replace('c', '')
   year_range = year_range.replace('Present', str(datetime.now().year))
-  parts = year_range.split(' - ')
+  parts = re.split(r' ?- ?', year_range)
+  #parts = year_range.split(' - ')
   assert 2 == len(parts)
   y1 = parts[0].replace('BCE', '')
   y2 = parts[1].replace('BCE', '')
@@ -39,17 +46,10 @@ def parse_year_range(year_range):
   return ys
 
 
-def contains_year_range(s):
-  """Checks if there is a year range in s."""
-  ms = [m for m in re.finditer(r'c?(Present|[0-9]+(BCE)?) - c?(Present|[0-9]+(BCE)?)', s)]
-  if not ms: return False
-  return True
-
-
 def parse_title(t):
   if t[-1] == '.': t = t[:-1]
   r = {}
-  parts = re.split(r'(c?(?:Present|[0-9]+(?:BCE)?) - c?(?:Present|[0-9]+(?:BCE)?))', t)
+  parts = re.split(r'(c?(?:Present|[0-9]+(?:BCE)?) ?- ?c?(?:Present|[0-9]+(?:BCE)?))', t)
   parts = [re.sub(r'^[ ,]*|[ ,]*$', '', p) for p in parts]
   parts = [p for p in parts if p]
 
@@ -61,18 +61,55 @@ def parse_title(t):
       if len(parts) > 3:
         r['dynasty_years'] = parse_year_range(parts[3])
 
+  if 'dynasty' not in r and ',' in r['name'] and 'Dynasty' in r['name']:
+    # ex: "China, Sui Dynasty 589 - 618"
+    name, dynasty = r['name'].split(', ')
+    r['name'] = name
+    r['dynasty'] = dynasty
+
+  if 'dynasty' in r and ',' in r['dynasty'] and 'Dynasty' in r['dynasty']:
+    # ex: "lost 1947, 1948., Mughal Dynasty"
+    m = re.search(r', ([^,]+ Dynasty)', r['dynasty'])
+    assert m, '%s : %s' % (t, r['dynasty'])
+    r['dynasty'] = m.group(1)
+
   return r
 
 
-# tests = [
-# "Magadha, 1000BCE - 733."
-# , "Chios"
-# , "Dasarna, 1000BCE - 100."
-# , "Indo Parthia, 87BCE - 45."
-# , "Persia, 646BCE - 1353, Achaemenid Dynasty 559 - 330BCE."
-# , "Magadha, 1000BCE - 733, Haryanka Dynasty c575 - c410BCE."
-# , "Africa, Autonomous Province, 800 - 909, Aghlabid Dynasty 800 - 909"
-# ]
+# TODO(danvk): turn this into a real test
+def test_parse_title():
+  parse_title_tests = [
+  "Magadha, 1000BCE - 733."
+  , "Chios"
+  , "Dasarna, 1000BCE - 100."
+  , "Indo Parthia, 87BCE - 45."
+  , "Persia, 646BCE - 1353, Achaemenid Dynasty 559 - 330BCE."
+  , "Magadha, 1000BCE - 733, Haryanka Dynasty c575 - c410BCE."
+  , "Africa, Autonomous Province, 800 - 909, Aghlabid Dynasty 800 - 909"
+  , "China, Sui Dynasty 589 - 618"
+  , "Egypt2, Autonomous Province, 1811 - 1882, Muhammad Ali Dynasty 1805 - 1953"
+  , "India, British Colony, gained 1763-1913, lost 1947, 1948., Mughal Dynasty"
+  ]
+  for t in parse_title_tests:
+    print t
+    print parse_title(t)
+
+
+def possessive_form(country):
+  m = {
+    'Norway': 'Norwegian',
+    'Venice': 'Venetian',
+    'Denmark': 'Danish',
+    'Genoa': 'Genoese',
+    'Byzantium': 'Byzantine',
+    'Turkey': 'Turkish',
+    'Savoy': 'Savoian',
+    'France': 'French',
+    'Romania': 'Rumania',
+    'Argentina': 'Argentine'
+  }
+  if country in m: return m[country]
+  return country
 
 
 class SvgFile(object):
@@ -103,7 +140,18 @@ class SvgFile(object):
       entry['name'] = ' '.join([x.string for x in t.contents[1:]])
 
       title_entry = parse_title(t.title.string)
-      assert title_entry['name'] == entry['name'], '"%s" vs "%s"' % (title_entry['name'], entry['name'])
+
+      # hack for "Africa, Autonomous Province"
+      #if ',' in title_entry['name']:
+      #  title_entry['name'] = title_entry['name'].split(',')[0]
+
+      title_name = title_entry['name']
+      entry_name = entry['name']
+      if title_name != entry_name:
+        if possessive_form(entry_name) not in title_name:
+          print '"%s" vs "%s" in %s' % (title_name, entry_name, self._filename)
+        entry['title_name'] = entry['name']
+
       for field in ['years', 'dynasty', 'dynasty_years']:
         if field in title_entry: entry[field] = title_entry[field]
 
